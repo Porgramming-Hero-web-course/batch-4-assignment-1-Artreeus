9518f4bda8aa5ae91b346bbf2bb286b36838f018
Artreeus <142108262+Artreeus@users.noreply.github.com>
2024-11-08 23:22:09 +0600

    problem 2 solved

--- problem-2.ts
+++ problem-2.ts
@@ -1,5 +1,18 @@
 function removeDuplicates(arr: number[]): number[] {
-    return [...new Set(arr)];
-  };
+  const seen: Set<number> = new Set();
+  const result: number[] = [];
 
-  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+  for (const num of arr) {
+    if (!seen.has(num)) {
+      seen.add(num);
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
+// console.log(removeDuplicates([1, 2, 9, 9, 2, 7, 7, 8, 8, 10, 2, 3, 4, 4, 5]));
+
+
+// i have used google to solve this problem 
